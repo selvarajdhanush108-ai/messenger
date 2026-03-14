b4200facd40be4ffe86bb75a54d661a1c0bde0c8
selvarajdhanush108-ai <selvarajdhanush108@gmail.com>
2026-03-14 13:34:50 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { findUser } = require("../controllers/userController");
+const { findUser, updateUser } = require("../controllers/userController");
+const protect = require("../middleware/authMiddleware");
+
+// IMPORTANT: Put /update BEFORE /:chatId so it doesn't get confused
+router.put("/update", protect, updateUser);
 
 router.get("/:chatId", findUser);
 
