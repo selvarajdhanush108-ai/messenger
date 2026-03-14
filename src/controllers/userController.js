e025a5cd9f90c58b1a189cd00b59eb7d588399b8
selvarajdhanush108-ai <selvarajdhanush108@gmail.com>
2026-03-14 13:34:32 +0530

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,6 @@
 const User = require("../models/User");
 
 exports.findUser = async (req, res) => {
-
   const { chatId } = req.params;
 
   const user = await User.findOne({ chatId });
@@ -11,5 +10,36 @@ exports.findUser = async (req, res) => {
   }
 
   res.json(user);
+};
 
+// NEW: Update user profile
+exports.updateUser = async (req, res) => {
+  try {
+    // req.user comes from our authMiddleware
+    const userId = req.user; 
+    const { username, email } = req.body;
+
+    // Check if the new email is already taken by someone else
+    if (email) {
+      const emailExists = await User.findOne({ email, _id: { $ne: userId } });
+      if (emailExists) {
+        return res.status(400).json({ message: "Email already in use by another account" });
+      }
+    }
+
+    // Update and return the new document
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { 
+        ...(username && { username }),
+        ...(email && { email }) 
+      },
+      { new: true, runValidators: true }
+    ).select("-password"); // Don't send the password back!
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error updating profile" });
+  }
 };
